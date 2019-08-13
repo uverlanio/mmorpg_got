89dfc0b36796139a2be1773c521f330fc51ab919
uverdeveloper <uverlanio@outlook.com>
2019-08-12 22:21:26 -0300

    Uso de express session, utilizando vars de sessao

--- mmorpg_got/app/models/UsuariosDAO.js
+++ mmorpg_got/app/models/UsuariosDAO.js
@@ -5,8 +5,33 @@ function UsuariosDAO(connection){
 UsuariosDAO.prototype.inserirUsuario = function(usuario){
 	this._connection.open(function(err, mongoclient){
 		mongoclient.collection("usuarios", function(err, collection){
-			console.log(usuario);
 			collection.insert(usuario);
+
+		mongoclient.close();
+		});
+	});
+
+}
+
+UsuariosDAO.prototype.autenticar = function(usuario, req, res){
+	this._connection.open(function(err, mongoclient){
+		mongoclient.collection("usuarios", function(err, collection){
+			collection.find(usuario).toArray(function(err, result){
+				
+				if(result[0] != undefined){
+					
+					req.session.autorizado = true;
+
+					req.session.usuario = result[0].usuario;
+					req.session.casa = result[0].casa;
+				}
+
+				if(req.session.autorizado){
+					res.redirect("jogo")
+				}else{
+					res.render("index", {validacao : {}})
+				}
+			});
 
 		mongoclient.close();
 		});
