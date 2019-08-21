7cb6987cc3c77ad31092be76cbea015238ab1b81
uverdeveloper <uverlanio@outlook.com>
2019-08-20 22:20:44 -0300

    Carregando os dados na view pergaminho

--- mmorpg_got/app/controllers/jogo.js
+++ mmorpg_got/app/controllers/jogo.js
@@ -5,11 +5,10 @@ module.exports.jogo = function(application, req, res){
 		return;
 	}
 
-	var comando_invalido = 'N';
+	var msg = '';
 
-	if(req.query.comando_invalido == 'S'){
-		comando_invalido = 'S';
-		console.log(comando_invalido);
+	if(req.query.msg != ''){
+		msg = req.query.msg;
 	}
 
 	var usuario = req.session.usuario;
@@ -18,7 +17,7 @@ module.exports.jogo = function(application, req, res){
 	var connection = application.config.dbConnection;
 	var JogoDAO = new application.app.models.JogoDAO(connection);
 
-	JogoDAO.iniciaJogo(res, usuario, casa, comando_invalido);
+	JogoDAO.iniciaJogo(res, usuario, casa, msg);
 }
 
 module.exports.sair = function(application, req, res){
@@ -44,8 +43,13 @@ module.exports.pergaminhos = function(application, req, res){
 		res.send('Usuário precisa fazer login');
 		return;
 	}
-		res.render('pergaminhos', {validacao : {}})
-	
+		//res.render('pergaminhos', {validacao : {}})
+		var connection = application.config.dbConnection;
+		var JogoDAO = new application.app.models.JogoDAO(connection);
+
+		var usuario = req.session.usuario;
+		JogoDAO.getAcoes(res, usuario);
+
 	}
 
 	module.exports.ordenar_acao_sudito = function(application, req, res){
@@ -58,10 +62,16 @@ module.exports.pergaminhos = function(application, req, res){
 		var erros = req.validationErrors();
 
 		if(erros){
-			res.redirect('jogo?comando_invalido=S');
+			res.redirect('jogo?msg=A');
 			return;
 		}
 
-		res.send('Tudo OK');
+		var connection = application.config.dbConnection;
+		var JogoDAO = new application.app.models.JogoDAO(connection);
+
+		dadosForm.usuario = req.session.usuario;
+		JogoDAO.acao(dadosForm);
+
+		res.redirect('jogo?msg=B');
 	
 	}
--- mmorpg_got/app/models/JogoDAO.js
+++ mmorpg_got/app/models/JogoDAO.js
@@ -21,11 +21,44 @@ JogoDAO.prototype.gerarParametros = function(usuario){
 
 }
 
-JogoDAO.prototype.iniciaJogo = function(res, usuario, casa, comando_invalido){
+JogoDAO.prototype.iniciaJogo = function(res, usuario, casa, msg){
 	this._connection.open(function(err, mongoclient){
 		mongoclient.collection("jogo", function(err, collection){
 			collection.find({usuario : usuario}).toArray(function(err, result){
-				res.render('jogo', {jogo: result[0] , img_casa : casa , comando_invalido : comando_invalido});			
+				res.render('jogo', {jogo: result[0] , img_casa : casa , msg : msg});			
+			});
+		mongoclient.close();
+			
+		});
+	});
+}
+
+JogoDAO.prototype.acao = function(acao){
+	this._connection.open(function(err, mongoclient){
+		mongoclient.collection("acao", function(err, collection){
+			
+			var tempo = null;
+			var date = new Date();	
+
+			switch(acao.acao){
+				case 1: tempo = 1 * 60 * 60000;
+				case 2: tempo = 2 * 60 * 60000;
+				case 3: tempo = 5 * 60 * 60000;
+				case 4: tempo = 5 * 60 * 60000;
+			}
+			acao.acao_termina_em = date.getTime() + tempo;
+			collection.insert(acao);
+		mongoclient.close();
+			
+		});
+	});
+}
+
+JogoDAO.prototype.getAcoes = function(res, usuario){
+	this._connection.open(function(err, mongoclient){
+		mongoclient.collection("acao", function(err, collection){
+			collection.find({usuario : usuario}).toArray(function(err, result){
+				res.render('pergaminhos', {acoes : result})			
 			});
 		mongoclient.close();
 			
