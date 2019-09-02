d36ece64956ba5fbe4296d3052210efa9462971e
usilva <usilva@a5solutions.com>
2019-09-02 15:00:44 -0300

    commit

--- mmorpg_got/app/models/JogoDAO.js
+++ mmorpg_got/app/models/JogoDAO.js
@@ -48,16 +48,37 @@ JogoDAO.prototype.acao = function(acao){
 			}
 			acao.acao_termina_em = date.getTime() + tempo;
 			collection.insert(acao);
-		mongoclient.close();
 			
 		});
-	});
+
+		mongoclient.collection("jogo", function(err, collection){
+			
+			var moedas = null;
+
+			switch(parseInt(acao.acao)){
+				case 1: moedas = -2 * acao.quantidade; break;
+				case 2: moedas = -3 * acao.quantidade; break;
+				case 3: moedas = -1 * acao.quantidade; break;
+				case 4: moedas = -1 * acao.quantidade; break;
+			}
+			
+			collection.update(
+				{usuario : acao.usuario},
+				{$inc:{moeda : moedas}}
+
+				);
+			mongoclient.close();
+			});
+		});
 }
 
 JogoDAO.prototype.getAcoes = function(res, usuario){
 	this._connection.open(function(err, mongoclient){
 		mongoclient.collection("acao", function(err, collection){
-			collection.find({usuario : usuario}).toArray(function(err, result){
+
+			var date = new Date();
+			var momento_atual = date.getTime();
+			collection.find({usuario : usuario, acao_termina_em: {$gt: momento_atual}}).toArray(function(err, result){
 				res.render('pergaminhos', {acoes : result})			
 			});
 		mongoclient.close();
